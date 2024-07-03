adf3844c1d3cb34a0c228da005193c55558aa74e
viperadnan-git <viperadnan@gmail.com>
2024-07-03 14:09:52 +0530

    Initial Commit

---
+++ iHospital/iHospitalApp.swift
@@ -0,0 +1,17 @@
+//
+//  iHospitalApp.swift
+//  iHospital
+//
+//  Created by Adnan Ahmad on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct iHospitalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
